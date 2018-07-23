8846e78e79906d52e9f5a6a57915555afdf4c531
Georgina Pacey <paceygeorgina@gmail.com>
2018-07-23 21:41:31 +0200

    register without passport

---
+++ controllers/users.controller.js
@@ -0,0 +1,35 @@
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+const User = require('../models/user.model');
+
+module.exports.create = (req, res, next) => {
+  res.render('users/signup');
+}
+
+module.exports.doCreate = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        res.render('users/signup', {
+          user: req.body,
+          errors: { email: 'Email already registered' }
+        });
+      } else {
+        user = new User (req.body);
+        return user.save()
+          .then(user => {
+            res.redirect('/sessions/create');
+          });
+      }
+    })
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.render('users/signup', {
+          user: req.body,
+          errors: error.errors
+        });
+      } else {
+        next(error);
+      }
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var usersController = require('../controllers/users.controller')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/signup', usersController.create);
+router.post('/signup', usersController.doCreate);
+
 
 module.exports = router;
