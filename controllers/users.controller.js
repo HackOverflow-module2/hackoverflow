0daa0234a9929801f8ca864bd3ba59e2c6f5c665
Georgina Pacey <paceygeorgina@gmail.com>
2018-08-01 20:18:21 +0200

    User-profile without edit

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,8 @@
 const createError = require('http-errors');
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
+const Question = require('../models/question.model');
+const Resource = require('../models/resource.model');
 
 module.exports.create = (req, res, next) => {
   res.render('users/signup');
@@ -35,8 +37,36 @@ module.exports.doCreate = (req, res, next) => {
 }
 
 module.exports.detail = (req, res, next) => {
-  const user = req.user._id
-  User.findById(user)
-    .then()
-    .catch()
+  const id = req.params.id
+  const userPromise = User.findById(id);
+  const questionPromise = Question.find({'user': id});
+  const resourcePromise = Resource.find({'user': id});
+    Promise.all([userPromise, questionPromise, resourcePromise])    
+      .then(([user, questions, resources]) => {
+      if (user) {
+        res.render('users/detail', {
+          user,
+          questions,
+          resources 
+        })
+      } else {
+        next(createError(404, 'user not found'));
+      }
+    })
+    .catch(error => next(error))
+}
+
+module.exports.edit = (req, res, next) => {
+  const id = req.params.id
+  User.findById(id)
+    .then(user => {
+      if(user) {
+        res.render('users/edit', {
+          user
+        })
+      } else {
+        next(createError(404, 'user not found'));
+      }
+    })
+    .catch(error => next(error))
 }
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var router = express.Router();
-var usersController = require('../controllers/users.controller')
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/users.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+const usersMiddleware = require('../middlewares/users.middleware');
+
 
 router.get('/signup', usersController.create);
 router.post('/signup', usersController.doCreate);
 
-router.get('/', usersController.detail)
+router.get(
+    '/:id',  
+    authMiddleware.isAuthenticated,
+    usersController.detail
+)
+
+router.get(
+    '/:id/edit', 
+    authMiddleware.isAuthenticated,
+    usersMiddleware.isOwner,
+    usersController.edit
+)
 
 module.exports = router;
