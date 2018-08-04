03649759af7dbebe1347f3409d779ce6d9fb00da
Georgina Pacey <paceygeorgina@gmail.com>
2018-08-04 19:56:42 +0200

    order question by rating

--- helpers/post.helper.js
+++ helpers/post.helper.js
@@ -1,13 +1,11 @@
-// module.exports = (hbs) => {
-//     hbs.registerHelper('eachReverse', function (arr, options) {
-//         return options.fn(this, {
-//             data: options.data,
-//             blockParams: arr.reverse()
-//         });
-//     });
-// }
-
 module.exports = (hbs) => {
+/*     hbs.registerHelper('eachByRating', function (arr, options) {
+        return options.fn(this, {
+            data: options.data,
+            blockParams: arr.reverse()
+        });
+    }); */
+
     hbs.registerHelper('date', function (date) {
         longDate = new Date(date);
         hour = longDate.getHours();
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,29 @@
 $( document ).ready(function() {
-    $( ".like" ).click(function() {
-        let rating
-    });
+    Order(".order-rating",".questions-wrap");
+
 });
+
+function Order(ratingButton, postsList) {
+    $( ratingButton ).click(function() {
+        let cont = $(postsList);
+    
+        let arr = $.makeArray(cont.children(".post-card"));
+    
+        arr.sort(function(a, b) {
+            var valA = +$(a).find('.rating-num').val();
+            var valB = +$(b).find('.rating-num').val();
+    
+            if (valA < valB) return 1;
+            if (valA > valB) return -1;
+    
+            return 0;
+        });
+    
+        cont.empty();
+    
+        $.each(arr, function() {
+            cont.append(this);
+        });
+    
+    });
+}
