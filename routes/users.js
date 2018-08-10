a57809a76a1b20681cf34aa5818c2fd3efdea56f
Georgina Pacey <paceygeorgina@gmail.com>
2018-08-10 22:12:47 +0200

    attempt at cloudinary

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
-const multer  = require('multer')
-const upload = multer({ dest: 'public/images/profile-photos' })
+const uploadCloud = require('../configs/cloudinary.config.js');
+//const multer  = require('multer')
+//const upload = multer({ dest: 'public/images/profile-photos' })
 
 
 router.get('/signup', usersController.create);
@@ -27,7 +28,7 @@ router.post(
     '/:id/edit',
     authMiddleware.isAuthenticated,
     authMiddleware.isOwner,
-    upload.single('photo'),
+    uploadCloud.single('photo'),
     usersController.doEdit
 )
 
